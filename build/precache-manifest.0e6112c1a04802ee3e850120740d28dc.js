8f16525eed3a434e3fd255fbd4e233d8a78345b1
Burhan <burhan@innoractive.com>
2019-02-12 22:35:58 +0800

    1. add build 2. remove it from gitignore

---
+++ build/precache-manifest.0e6112c1a04802ee3e850120740d28dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "28b94f9f03c6add818c3",
+    "url": "/static/js/main.28b94f9f.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "28b94f9f03c6add818c3",
+    "url": "/static/css/main.b4d6edc8.chunk.css"
+  },
+  {
+    "revision": "162ce8e16db9e12a4f18aa37136a2a39",
+    "url": "/index.html"
+  }
+];
