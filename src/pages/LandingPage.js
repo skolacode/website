14c559baf2dcd8056a25b259c2dce0a7818ae84d
Burhan <burhan@innoractive.com>
2019-02-28 00:11:25 +0800

    1. added a semantic ui 2. create a dummy nav bar

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
 
-class LandingPage extends Component {
+export default class LandingPage extends Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
+    const { activeItem } = this.state
+
     return (
-      <div>
-        <h1>Hello skola code!!!</h1>
-      </div>
-    );
+      <Menu>
+        <Menu.Item
+          name='editorials'
+          active={activeItem === 'editorials'}
+          onClick={this.handleItemClick}
+        >
+          Editorials
+        </Menu.Item>
+
+        <Menu.Item name='reviews' active={activeItem === 'reviews'} onClick={this.handleItemClick}>
+          Reviews
+        </Menu.Item>
+
+        <Menu.Item
+          name='upcomingEvents'
+          active={activeItem === 'upcomingEvents'}
+          onClick={this.handleItemClick}
+        >
+          Upcoming Events
+        </Menu.Item>
+      </Menu>
+    )
   }
 }
-
-export default LandingPage;
